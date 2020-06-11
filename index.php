1696dca4119c2c481a9095ef83a3c8594556542f
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-11 16:08:33 -0400

    add comments describing changes that have been made
    includes bug fixes and general improvements, including updates to move toward PSR-2 compliance

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+// PSR-2 Update: converted tabs to spaces
 
+// Added a Namespace
 namespace eBaseInterviewExercise;
 
 class Interview
 {
+    // BUG FIX: this should be a static variable based on usage below
     public static $title = 'Interview test';
 }
 
@@ -17,6 +20,7 @@ $people = array(
     array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
 
+// BUG FIX: check to see if the 'person' index exists; set to NULL if it doesn't
 $person = isset($_POST['person']) ? $_POST['person'] : null;
 
 ?>
@@ -35,8 +39,10 @@ $person = isset($_POST['person']) ? $_POST['person'] : null;
     <h1><?=Interview::$title;?></h1>
 
     <?php
+    // BUG FIX: this was an infinite loop, starting at 10 and increasing to infinity; now it iterates from 0 to 9, resulting in 10 iterations
     // Print 10 times
     for ($i = 0; $i < 10; $i++) {
+        // BUG FIX: substituted in the correct concatenation operator in place of +
         echo '<p>' . $lipsum . '</p>';
     }
     ?>
@@ -45,6 +51,7 @@ $person = isset($_POST['person']) ? $_POST['person'] : null;
     <hr>
 
 
+    <?php // BUG FIX: updated the method from GET to POST, since POST is commonly used for submitting/sending data, and POST doesn't display those values in the URL's query string ?>
     <form method="post" action="<?=$_SERVER['REQUEST_URI'];?>">
         <p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
         <p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
@@ -52,6 +59,10 @@ $person = isset($_POST['person']) ? $_POST['person'] : null;
         <p><input type="submit" value="Submit" /></p>
     </form>
 
+    <?php
+        // PSR-2 Update: PSR-2 requires a space after the closing bracket
+        // BUG FIX: check if the $person variable exists
+    ?>
     <?php if (isset($person)) : ?>
         <p><strong>Person</strong> <?=$person['first_name'] ?? '';?>, <?=$person['last_name'] ?? '';?>, <?=$person['email'] ?? '';?></p>
     <?php endif; ?>
@@ -69,8 +80,10 @@ $person = isset($_POST['person']) ? $_POST['person'] : null;
             </tr>
         </thead>
         <tbody>
+            <?php // PSR-2 Update: PSR-2 requires a space after the closing bracket ?>
             <?php foreach ($people as $person) : ?>
                 <tr>
+                    <?php // BUG FIX: previous code was treating $person as an object instead of an array ?>
                     <td><?=$person['first_name'];?></td>
                     <td><?=$person['last_name'];?></td>
                     <td><?=$person['email'];?></td>
